26c53da02deef25e6e4889c6f5b920fa12eee2bb
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-30 17:45:33 -0300

    Video #30 Ajustado tamanho e alinhamento

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -5,7 +5,27 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Image.asset('assets/images/logo.png'),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              children: [
+                Expanded(child: DropdownButton(items: [])),
+                Expanded(
+                  child: TextField(
+                      decoration: InputDecoration(
+                    labelText: '',
+                  )),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Converter'),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
