ccfb6970aeccf8bb2b6f710338377e20d4756e11
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-30 16:20:10 -0300

    Video #30 criado pastas, iniciado  home View

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_conversor_moeda/app/views/home_view.dart';
 
+main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomeView(),
+    );
+  }
+}
